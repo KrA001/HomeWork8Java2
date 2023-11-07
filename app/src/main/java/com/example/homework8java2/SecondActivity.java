8aa0f09a9b2f9575fe2169f5ad079f22c4515565
LENOVO <test23062000@gmail.com>
2023-11-07 16:40:14 +0300

    Initial commit

---
+++ app/src/main/java/com/example/homework8java2/SecondActivity.java
@@ -0,0 +1,28 @@
+package com.example.homework8java2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class SecondActivity extends AppCompatActivity {
+    private ImageView iconBack;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        iconBack = this.findViewById(R.id.iv_arraw_back);
+
+        goBack();
+    }
+
+    private void goBack() {
+        iconBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "HomeWork8Java2"
+include(":app")
+ 
